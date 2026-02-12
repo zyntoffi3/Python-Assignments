b8d12d00af9e2f17e2a2ced64f4cf78a5f086554
zyntoffi3 <hiligan.jonahmae0o14@gmail.com>
2026-02-13 07:58:25 +0800

    Add personal_info.py with user details and types

---
+++ Activity1/personal_info.py
@@ -0,0 +1,17 @@
+fullname = "Jonah Mae Hiligan"
+age = 21
+height = 1.60
+is_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("\nAge:", age)
+print(type(age))
+
+print("\nHeight in meters:", height)
+print(type(height))
+
+print("\nCurrently working?", is_working)
+print(type(is_working))
+
